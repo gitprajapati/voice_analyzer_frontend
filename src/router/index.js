3b97a149bde97676443d0f97830247481a358660
gitprajapati <21f1003546@student.onlinedegree.iitm.ac.in>
2024-08-21 22:30:57 +0530

    route set

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,34 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import auth from "./auth";
 import LoginPage from "@/components/loginPage.vue";
 import dashBoard from "@/components/dashBoard.vue";
 import voiceAnalyzer from "@/components/voiceAnalyzer.vue";
 import liveTranscriber from "@/components/liveTranscriber.vue";
 import userHistory from "@/components/userHistory.vue";
 import transcriptionAnalysis from "@/components/transcriptionAnalysis.vue";
+
 // Define your routes
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    // Define a route for the root path
+    redirect: () => {
+      // Check if the user is authenticated
+      if (auth.loggedIn === "true") {
+        // If authenticated, redirect to the dashboard
+        return { name: "dashboard" };
+      } else {
+        // If not authenticated, redirect to the login page
+        return { name: "login" };
+      }
+    },
+  },
   {
     path: "/dashboard",
     name: "dashboard",
     component: dashBoard,
+    meta: { requiresAuth: true },
   },
   {
     path: "/login",
@@ -22,21 +39,25 @@ const routes = [
     path: "/analyzer",
     name: "analyzer",
     component: voiceAnalyzer,
+    meta: { requiresAuth: true },
   },
   {
     path: "/transcription",
     name: "transcription",
     component: liveTranscriber,
+    meta: { requiresAuth: true },
   },
   {
     path: "/user-transcriptions-history",
     name: "user-transcriptions-history",
     component: userHistory,
+    meta: { requiresAuth: true },
   },
   {
     path: "/word-frequencies",
     name: "word-frequencies",
     component: transcriptionAnalysis,
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -44,6 +65,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(), // Use HTML5 history mode
   routes,
+});
+
+// Navigation guard to check authentication
+router.beforeEach((to, from, next) => {
+  // Check if the route requires authentication
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Check if user is authenticated
+    if (auth.loggedIn === "true") {
+      next(); // User is authenticated, allow access
+    } else {
+      next("/login"); // User is not authenticated, redirect to login
+    }
+  } else {
+    next(); // Route does not require authentication, allow access
+  }
 });
 
 // Export the router instance
